8bb8f221c5037020cfe2e1ae762d6b3817426acc
Vinicius Dias <vinicius.dias@achievers.com>
2020-10-09 13:31:09 -0300

    Removendo analytics (usando GTM) e alterando script de deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-41215252-5`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
