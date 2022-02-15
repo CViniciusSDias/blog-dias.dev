adf75b4db6ddb0c1cb494c2dfda96b25696ebba2
Vinicius Dias <carlosv775@gmail.com>
2022-02-15 12:58:11 -0300

    Removendo adsense

--- publish.sh
+++ publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-./vendor/bin/sculpin generate --env=prod
+docker run --rm -itv $(pwd):/app -w /app php-blog php ./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+yarn run deploy
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
