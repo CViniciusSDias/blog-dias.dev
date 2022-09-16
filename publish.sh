279d348c499436671c9c04f96727ab4d50acc67d
Vinicius Dias <carlosv775@gmail.com>
2022-09-16 15:22:28 -0300

    Adicionando usuário ao comando de sculping generate

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -itv $(pwd):/app -w /app php-blog php ./vendor/bin/sculpin generate --env=prod
+docker run --rm -itv $(pwd):/app -u $(id -u):$(id -g) -w /app php-blog php ./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 yarn run deploy
