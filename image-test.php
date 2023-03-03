762974c0f0461521073347f6a5c74b249e3d68ec
Vinicius Dias <carlosv775@gmail.com>
2023-03-03 01:18:39 -0300

    Deixando o arquivo minimamente legível

--- image-test.php
+++ image-test.php
@@ -5,40 +5,68 @@ declare(strict_types=1);
 $files = glob(__DIR__ . '/source/_posts/*.md');
 
 foreach ($files as $file) {
-    $filePointer = fopen($file, 'r');
-    fseek($filePointer, 4, SEEK_SET);
-    $title = json_decode(str_replace('title: ', '', trim(fgets($filePointer))));
-    fclose($filePointer);
+    $title = getTitle($file);
 
     $image = imagecreate(1200, 630);
-    $white = imagecolorallocate($image, 82, 193, 150);
-    imagefill($image, 0, 0, $white);
+    $backgroundColor = imagecolorallocate($image, 82, 193, 150);
+    imagefill($image, 0, 0, $backgroundColor);
 
-    $black = imagecolorallocate($image, 255, 255, 255);
+    $textColor = imagecolorallocate($image, 255, 255, 255);
 
     $iteration = 0;
     do {
-        $text = $title;
-
-        if (strlen($text) > 35) {
-            $shortened = substr($title, 0, 34);
-            $lastSpace = strrpos($shortened, ' ');
-            $text = substr($shortened, 0, $lastSpace);
-        }
-        $fontSize = 58;
-        imagettftext(
-            $image,
-            $fontSize,
-            0,
-            10,
-            330 + $iteration++ * ($fontSize + 10),
-            $black,
-            __DIR__ . '/source/assets/fonts/Baloo2-Bold.ttf',
-            $text
-        );
+        $text = getLineFromTitle($title);
+        writeLineToImage($image, $iteration++, $textColor, $text);
 
         $title = trim(substr($title, strlen($text)));
     } while ($title);
 
     imagepng($image, __DIR__ . '/source/assets/img/thumbs/' . basename($file, '.md') . '.png');
+
+    imagedestroy($image);
+}
+
+function getTitle(string $file): mixed
+{
+    $filePointer = fopen($file, 'r');
+    fseek($filePointer, 4, SEEK_SET);
+
+    $titleLine = trim(fgets($filePointer));
+    $title = str_replace('title: ', '', $titleLine);
+    $decodedTitle = json_decode($title);
+
+    fclose($filePointer);
+
+    return $decodedTitle;
+}
+
+function getLineFromTitle(string $title): string
+{
+    if (strlen($title) <= 35) {
+        return $title;
+    }
+
+    $limitedText = substr($title, 0, 34);
+    $lastSpacePosition = strrpos($limitedText, ' ');
+
+    return substr($limitedText, 0, $lastSpacePosition);
+}
+
+function writeLineToImage(GdImage $image, int $multiplier, int $textColor, string $text): void
+{
+    $fontSize = 58;
+    $lineSpacing = 10;
+    $leftStartPosition = 10;
+    $topStartPosition = 330;
+
+    imagettftext(
+        $image,
+        $fontSize,
+        0,
+        $leftStartPosition,
+        $topStartPosition + $multiplier * ($fontSize + $lineSpacing),
+        $textColor,
+        __DIR__ . '/source/assets/fonts/Baloo2-Bold.ttf',
+        $text
+    );
 }
