83548e13107d8973f7c8fb8d0b422b3a92eacb33
Vinicius Dias <carlosv775@gmail.com>
2023-12-18 18:43:10 -0300

    Corrigindo processo de build

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+docker run --rm -itv $(pwd):/app -u $(id -u):$(id -g) -w /app php-blog php thumbnails-generator.php
+if [ $? -ne 0 ]; then echo "Erro ao gerar thumbs"; exit 1; fi
+
 yarn encore prod
 if [ $? -ne 0 ]; then echo "Could not build the assets"; exit 1; fi
 
 docker run --rm -itv $(pwd):/app -u $(id -u):$(id -g) -w /app php-blog php ./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
-
-docker run --rm -itv $(pwd):/app -u $(id -u):$(id -g) -w /app php-blog php thumbnails-generator.php
-if [ $? -ne 0 ]; then echo "Erro ao gerar thumbs"; exit 1; fi
 
 yarn run deploy
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
